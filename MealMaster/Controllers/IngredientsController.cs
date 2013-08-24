f56d4516d5db8d4ca5e0cd99a7878334cd46cfb5
Jonas Bruhn Christensen <jonasbc@msn.com>
2013-08-24 23:50:58 +0200

    Added ingredient form

---
+++ MealMaster/Controllers/IngredientsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using MealMaster.Models;
+
+namespace MealMaster.Controllers
+{
+    public class IngredientsController : Controller
+    {
+        //
+        // GET: /Ingredients/
+
+        public ActionResult IngredientsList()
+        {
+            return View();
+        }
+
+        public ActionResult CreateEditIngredient()
+        {
+            return View();
+        }
+
+        public void CreateIngredient(IngredientModel ingredient)
+        {
+            
+        }
+
+    }
+}
---
+++ MealMaster/Controllers/RecipesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MealMaster.Controllers
+{
+    public class RecipesController : Controller
+    {
+        //
+        // GET: /Recipes/
+
+        public ActionResult RecipesList()
+        {
+            return View();
+        }
+
+    }
+}
