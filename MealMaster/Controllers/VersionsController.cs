127f8a398e8510906305247d70fac9d72976b942
Jonas Bruhn Christensen <jonasbc@msn.com>
2013-10-29 22:29:32 +0100

    Added version history and renamed style sheet

---
+++ MealMaster/Controllers/VersionsController.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Web.Mvc;
+using System.Xml.Serialization;
+using MealMaster.Models;
+
+namespace MealMaster.Controllers
+{
+    public class VersionsController : Controller
+    {
+        //
+        // GET: /Versions/
+
+        public ActionResult VersionNotes()
+        {
+            var path = Server.MapPath("~/VersionNotes.xml");
+
+            var serializer = new XmlSerializer(typeof (VersionCollection));
+
+            var reader = new StreamReader(path);
+
+            var versions = (VersionCollection) serializer.Deserialize(reader);
+
+            reader.Close();
+
+            var versionList = versions.Version;
+
+            return View(versionList);
+        }
+
+    }
+}
---
+++ MealMaster/Models/Version.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace MealMaster.Models
+{
+    [Serializable()]
+    public class Version
+    {
+        [XmlElement("Number")]
+        public string Number { get; set; }
+
+        [XmlElement("Description")]
+        public string Description { get; set; }
+    }
+
+    [Serializable()]
+    [XmlRoot("VersionCollection")]
+    public class VersionCollection
+    {
+        [XmlArray("Versions")]
+        [XmlArrayItem("Version", typeof(Version))]
+        public Version[] Version { get; set; }
+    }
+}
